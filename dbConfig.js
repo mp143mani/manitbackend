cdd7cb477d3bf96c50ef7e094e1b42c0663f1ae7
mp143mani <127532509+mp143mani@users.noreply.github.com>
2023-10-18 13:58:50 +0530

    s

---
+++ dbConfig.js
@@ -0,0 +1,5 @@
+const mongodb = require('mongodb')
+const dbName = 'Zen'
+const dbUrl = `mongodb+srv://mani143tech:mani4213@cluster0.ckxmigv.mongodb.net/${dbName}`
+const MongoClient=mongodb.MongoClient
+module.exports ={mongodb,dbName,dbUrl,MongoClient}
